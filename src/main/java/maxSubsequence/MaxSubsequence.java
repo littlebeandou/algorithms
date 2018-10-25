d8d42972ba4faf0ced1dfa8589de9fe0b4ff678f
xiaoqiang <xiaoqiang@126.com>
2018-10-25 11:27:50 +0800

    测试了求最大子序列的算法

---
+++ src/main/java/maxSubsequence/MaxSubsequence.java
@@ -0,0 +1,35 @@
+package maxSubsequence;
+
+/**
+ * @author xiaoqiang
+ * @date 2018-10-25 11
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 利用不同的算法求解最大子序列问题
+ */
+public class MaxSubsequence {
+
+    static class Node{
+
+    }
+
+
+    /**
+     * 普通的穷举方法
+     * @param source
+     * @return
+     */
+    public static int[] exhaustiveMethod(int[] source){
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
